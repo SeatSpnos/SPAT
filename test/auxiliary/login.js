45be180c9a5007d40e1dd390959edd71f88bc1ba
SeatSpnos <segmento.especializado@gmail.com>
2017-07-19 15:12:18 +0100

    Update login.js

--- test/auxiliary/login.js
+++ test/auxiliary/login.js
@@ -1,7 +1,7 @@
 module.exports = function (request, next) {
   request
     .post('/login')
-    .send({ username: 'fcrespim', password: 'BVL_pc89' })
+    .send({ username: 'fcrespim', password: '' })
     .end(function (error, response) {
       if (error) next(error);
       next(null, response.headers['set-cookie']);
