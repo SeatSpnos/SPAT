d9f5c5098ae5b6855491ed69b4016b17e2546bf1
SeatSpnos <segmento.especializado@gmail.com>
2017-07-19 15:20:07 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ var mysql      = require('mysql')
 var connection = mysql.createConnection({
   'host'      : 'localhost',
   'user'      : 'root',
-  'password': 'TNQIDKFA',
+  'password': '',
   'multipleStatements': true,
   'dateStrings': 'date'
 })
